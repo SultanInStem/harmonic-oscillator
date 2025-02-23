752fb542c135b3f124bcddff5076294e1dd70296
= <sultaninstem004@gmail.com>
2025-02-23 13:23:35 -0800

    minor

--- canvas.py
+++ canvas.py
@@ -6,6 +6,10 @@ class Canvas:
         pygame.init()
         self.running = True
         self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Simple Harmonic Oscillator")
+
+        self.clock = pygame.time.Clock()
+        self.fps = 60
 
     def handle_events(self): 
         for event in pygame.event.get(): 
@@ -14,10 +18,10 @@ class Canvas:
     def update(self): 
         pass 
     def render(self): 
-
-
+        self.screen.fill((0,0,0))
 
         pygame.display.flip()
+        self.clock.tick(self.fps)
 
     def run(self): 
         while(self.running): 
