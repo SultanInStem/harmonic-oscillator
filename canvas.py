46a638cc2ffdd27bd5a7bea370a3398b0a947a18
= <sultaninstem004@gmail.com>
2025-02-23 13:21:13 -0800

    setup

--- canvas.py
+++ canvas.py
@@ -1,14 +1,28 @@
 import pygame 
-
+import sys 
 
 class Canvas: 
     def __init__(self): 
         pygame.init()
-
+        self.running = True
+        self.screen = pygame.display.set_mode()
 
     def handle_events(self): 
-        pass 
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                self.running = False
     def update(self): 
         pass 
     def render(self): 
-        pass 
+
+
+
+        pygame.display.flip()
+
+    def run(self): 
+        while(self.running): 
+            self.handle_events()
+            self.update()
+            self.render()
+        pygame.quit()
+        sys.exit()
