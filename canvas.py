4ca6885c598aa40479e6349cb22b7cf18e8387e1
= <sultaninstem004@gmail.com>
2025-02-23 12:22:51 -0800

    setup

---
+++ canvas.py
@@ -0,0 +1,14 @@
+import pygame 
+
+
+class Canvas: 
+    def __init__(self): 
+        pygame.init()
+
+
+    def handle_events(self): 
+        pass 
+    def update(self): 
+        pass 
+    def render(self): 
+        pass 
