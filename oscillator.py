3434f485aff81dea9765fadc2aeddddb36f26f2d
= <sultaninstem004@gmail.com>
2025-02-23 17:44:30 -0800

    minor

--- oscillator.py
+++ oscillator.py
@@ -1,5 +1,6 @@
 import pygame 
 from globals import to_screen, to_math
+import math
 class Oscillator: 
     def __init__(self, equilibrium_pos, spring_constant, block_mass, origin, size): 
         self.equilibrium_pos = equilibrium_pos 
@@ -11,9 +12,14 @@ class Oscillator:
 
 
     def draw(self, screen): 
+
+        ### Drawing the block 
+
         screen_x = to_screen(self.pos)[0]
         screen_y = to_screen(self.pos)[1]
         pygame.draw.rect(screen, (255,0,0),(screen_x, screen_y, self.block_size[0], self.block_size[1]), 0)
+
+        ### Drawing the spring 
 
     def move(self): 
         pass 
