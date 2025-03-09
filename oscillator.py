fd71068bdde16ef3a931e02e19deac463735d5d4
= <sultaninstem004@gmail.com>
2025-03-08 20:00:55 -0800

    dragging fixed

--- oscillator.py
+++ oscillator.py
@@ -27,8 +27,6 @@ class Oscillator:
         p = to_math(point)
         pos = self.pos 
         size = self.block_size
-        print(pos)
-        print(p)
         if p[0] >= pos[0] and p[0] <= pos[0] + size[0] and p[1] <= pos[1] and p[1] >= pos[1] - size[1]:
             return True
         return False 
